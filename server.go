720d08855cac4b3533b86be8d1577eca6bb87a55
dash <dash@Dash>
2024-07-03 19:22:30 +0300

    complete the code

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	http.HandleFunc("/hello", helloFunc)
+	http.HandleFunc("/no", NoFunc)
+	add := "localhost:8080"
+	err := http.ListenAndServe(add, nil)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func helloFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "oops", http.StatusNotFound)
+	}
+	writerresponse(w, "hello man!")
+}
+
+func NoFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "oops", http.StatusNotFound)
+	}
+	writerresponse(w, "no one here")
+
+}
+func writerresponse(w http.ResponseWriter, responseString string) {
+
+	response := []byte(responseString)
+	_, err := w.Write(response)
+	if err != nil {
+		panic(err)
+	}
+}
